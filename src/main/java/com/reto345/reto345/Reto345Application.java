656ddc5c7416c4f062297a663fe3cdebd90f6eb6
michael971103 <fincasvilla@gmail.com>
2022-10-13 11:36:18 -0500

    first commit

---
+++ src/main/java/com/reto345/reto345/Reto345Application.java
@@ -0,0 +1,13 @@
+package com.reto345.reto345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto345Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto345Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto345/reto345/Reto345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto345.reto345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto345ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
